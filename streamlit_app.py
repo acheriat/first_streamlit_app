1c1519534c0831264ec44b0f3450f33e94edaa97
acheriat <ahmed.cheriat@gmail.com>
2023-09-15 11:50:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+Streamlit.header('Breakfast Menu')
+Streamlit.header('Omega 3')
+Streamlit.header('Kale, Spinach')
+Streamlit.header('Hard-Boild Free-Range Egg')
