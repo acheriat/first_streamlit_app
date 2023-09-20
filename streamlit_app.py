0b6cbbcd481d8c85c7b113e9ddfd6212608c8ca8
acheriat <ahmed.cheriat@gmail.com>
2023-09-20 14:25:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,5 @@ streamlit.text(fruityvice_response.json())
 
 #fruityvice normalized
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.text(fruityvice_normalized)
 streamlit.dataframe(fruityvice_normalized)
