79e67419b75bbdfda69fa881bebbdc36b7ba9265
acheriat <ahmed.cheriat@gmail.com>
2023-09-20 14:18:01 +0200

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,5 @@ streamlit.dataframe(fruit_to_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+streamlit.text("ajout text libre")
