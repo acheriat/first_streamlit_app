851f104649505ce1c7898ec911765f4ddbf4f9ba
acheriat <ahmed.cheriat@gmail.com>
2023-09-23 20:09:45 +0200

    load some data

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 streamlit.text("toto")
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains::")
+streamlit.text(my_data_row)
